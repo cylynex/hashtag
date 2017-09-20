1469c52cfd20c7cc3c30015bb1c97c2dc5ed6df0
Santo Commarato <cylynex@gmail.com>
2017-09-20 16:28:54 -0600

    repaired missing file

---
+++ inc/database.php
@@ -0,0 +1,217 @@
+<?php
+
+// DB Class
+
+
+class Database {
+	
+	public $connection;
+	
+	function __construct() {
+		$this->dbconnect();
+	}
+	
+	public function dbconnect() {
+		
+		$this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+		if($this->connection->connect_errno) {
+			die("Database connection failed." . $this->connection->connect_error);
+		}
+		
+	}
+	
+	
+	// Query
+	public function query($sql) {		
+		$result = $this->connection->query($sql);
+		$this->confirm_query($result);
+		return $result;
+	}
+	
+	
+	// Confirm the query result
+	private function confirm_query($result) {
+		if (!$result) {
+			die ("Query Failed: ".$this->connection->error);
+		}
+	}
+	
+	
+	// Escape 
+	public function escape_string($string) {
+		$escaped_string = $this->connection->real_escape_string($string);
+		return $escaped_string;
+	}
+	
+	
+	// Get insert ID
+	public function the_insert_id() {
+		return mysqli_insert_id($this->connection);
+	}
+			
+}
+
+// Instantiate
+$database = new Database();
+
+
+class DatabaseObjects {
+	// Find all abstract
+	public static function find_all() {
+		return static::query("SELECT * FROM ". static::$db_table );
+	}
+	
+	// Find by ID abstract
+	public static function find_by_id($id,$fieldname) {
+		global $database;
+		$the_result_array = static::query("SELECT * FROM ". static::$db_table ." WHERE $fieldname = '$id' LIMIT 1 ");
+		return !empty($the_result_array) ? array_shift($the_result_array) : false;
+		
+	}
+	
+	
+	// Find query
+	public static function query($sql) {
+		global $database;
+		$out = $database->query($sql);
+		// instantiate
+		$obj = array();
+		while ($row = mysqli_fetch_array($out)) {
+			$obj[] = static::instantiation($row);
+		}
+				
+		return $obj;
+	}
+	
+	
+	// Num rows
+	public static function numrows($sql) {
+		$out = mysqli_num_rows($sql);
+	}
+	
+	
+	// Method to instantiate the class.
+	private static function instantiation($ubid) {
+		
+		$calling_class = get_called_class();
+		$the_object = new $calling_class;
+	
+		foreach ($ubid AS $the_attribute => $value) {
+			/*if ($the_object->has_the_attribute($the_attribute)) {
+				$the_object->$the_attribute = $value;
+			}*/
+			$the_object->$the_attribute = $value;
+			
+		}
+		
+		return $the_object;
+	}
+	
+	
+	// find attribute 
+	private function has_the_attribute($the_attribute) {
+		$object_properties = get_object_vars($this);  //PHP function
+		array_key_exists($the_attribute,$object_properties); // PHP function too	
+	}
+		
+	
+	// Insert into DB
+	public function create() {
+		global $database;
+		
+		// get all the properties
+		$properties = $this->clean_properties();
+		
+		$sql = "INSERT INTO ". static::$db_table ."(". implode(',', array_keys($properties)) .")";
+		$sql .= " VALUES ('". implode("','", array_values($properties)) ."')";		
+		
+		
+		// Run the query and return
+		if ($database->query($sql)) {
+			// success
+			return true;
+		} else {
+			// fail
+			return false;
+		}
+		
+	}
+	
+	
+	// Check for duplicate data in a table (ABS)
+	public function check_duplicate_data($sql) {
+		$query = "SELECT * FROM " . static::$db_table . " WHERE ";
+		$query .= $sql;
+		if(!empty($this->query($query))) { 
+			// Data already exists, fail
+			return false; 
+		} else { 
+			// Data doesnt exist, ok to add
+			return true;
+		}
+	}
+	
+	
+	// Update (not complete, but not used either so...)
+	public function update($idfield) {
+		global $database;
+		
+		$properties = $this ->properties();
+		$properties_pairs = array();
+		
+		foreach($properties AS $key => $value) {
+			$properties_pairs[] = "{$key}='{$value}' ";
+		}
+		
+		$sql = "UPDATE ".static::$db_table . " SET ";
+		$sql .= implode(", ", $properties_pairs);
+		$sql .= " WHERE $idfield = ". $database->escape_string($this->$idfield);
+		// echo "SQL query: $sql<br>";
+				
+		$database->query($sql);
+		//return (mysqli_affected_row($database->connection) == 1) ? true : false;
+		
+	}
+	
+	
+	// Delete
+	public function delete($idfield) {
+		global $database;
+		
+		$sql = "DELETE FROM ". static::$db_table ." WHERE $idfield  = '".$database->escape_string($this->$idfield)."' ";		
+		$database->query($sql);		
+		return true;
+	}
+	
+	
+	protected function clean_properties() {
+		global $database;
+		
+		$clean_properties = array();
+		
+		foreach ($this->properties() AS $key => $value) {
+			$clean_properties[$key] = $database->escape_string($value);
+		}
+		return $clean_properties;
+		
+	}
+	
+	
+	// abstration for the properties (not really used either)
+	protected function properties() {
+		// return get_object_properties($this);
+		$properties = array();
+		foreach (static::$db_table_fields AS $db_field) {
+			if (property_exists($this, $db_field)) {
+				$properties[$db_field] = $this->$db_field;
+			}
+		}
+		
+		return $properties;
+	}
+	
+
+}
+
+// Instantiate the DB
+$db = new DatabaseObjects();
